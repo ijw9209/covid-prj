a9112ecebf8b875e23fbce7edcbe69cfc6ce8e30
ijw9209 <ijw9209@naver.com>
2022-02-07 22:46:45 +0900

    covid UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/common.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cv-nav-fixed">
+      <nav className="cv-topnav bg-dark">
+        <a class="navbar-brand ps-3" href="index.html">Covid Helper</a>
+
+      </nav>
+
     </div>
   );
 }
